a4edaa8458d08888adfeb922cb37a9d82e25dfcb
PhlipTung <philip.dongfei@gmail.com>
2019-02-07 22:01:50 +0800

    Chapter 11.2, 11.3 & Submodules in Integration Test???

---
+++ Chapter11/adder/tests/common/mod.rs
@@ -0,0 +1,7 @@
+use adder;
+
+pub fn setup(){
+    // setup code specific to your library's tests would go here
+    assert_eq!(7, adder::add_two(5));
+
+}
--- Chapter11/test11_12/src/lib.rs
+++ Chapter11/test11_12/src/lib.rs
@@ -5,26 +5,12 @@ pub fn add_two(a: i32) -> i32 {
 fn internal_adder(a: i32, b: i32) -> i32 {
     a + b
 }
-
-
 #[cfg(test)]
 mod tests {
     use super::*;
-    /*
-    #[test]
-    fn exploration() {
-        assert_eq!(2 + 2, 4);
-    }
-    #[test]
-    fn another() {
-        panic!("Make this test fail");
-    }
-    */
-    /*
+
     #[test]
     fn internal() {
         assert_eq!(4, internal_adder(2,2));
     }
-    */
 }
-
